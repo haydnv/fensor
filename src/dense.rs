b6e80e91de5b58cee7bd59f12f800c6185cb450b
Haydn Vestal <haydnv@gmail.com>
2023-05-02 21:29:35 +0530

    first draft of impl SparseAccessor for SparseTable

---
+++ src/dense.rs
@@ -0,0 +1,159 @@
+use std::marker::PhantomData;
+
+use async_trait::async_trait;
+use destream::de;
+use freqfs::{DirLock, FileLoad};
+use futures::stream::{self, Stream, StreamExt, TryStreamExt};
+use ha_ndarray::*;
+use safecast::AsType;
+
+use super::Error;
+
+const IDEAL_BLOCK_SIZE: usize = 65_536;
+
+pub struct Array<T> {
+    data: Vec<T>,
+}
+
+struct ArrayVisitor<T> {
+    data: Vec<T>,
+}
+
+impl<T> ArrayVisitor<T> {
+    fn new() -> Self {
+        Self {
+            data: Vec::with_capacity(IDEAL_BLOCK_SIZE * 2),
+        }
+    }
+}
+
+macro_rules! decode_array {
+    ($t:ty, $name:expr, $decode:ident, $visit:ident) => {
+        #[async_trait]
+        impl de::Visitor for ArrayVisitor<$t> {
+            type Value = Array<$t>;
+
+            fn expecting() -> &'static str {
+                $name
+            }
+
+            async fn $visit<A: de::ArrayAccess<$t>>(
+                self,
+                mut array: A,
+            ) -> Result<Self::Value, A::Error> {
+                const BUF_SIZE: usize = 4_096;
+                let mut data = self.data;
+
+                let mut buf = [<$t>::zero(); BUF_SIZE];
+                loop {
+                    let len = array.buffer(&mut buf).await?;
+                    if len == 0 {
+                        break;
+                    } else {
+                        data.extend_from_slice(&buf[..len]);
+                    }
+                }
+
+                Ok(Array { data })
+            }
+        }
+
+        #[async_trait]
+        impl de::FromStream for Array<$t> {
+            type Context = ();
+
+            async fn from_stream<D: de::Decoder>(
+                _cxt: (),
+                decoder: &mut D,
+            ) -> Result<Self, D::Error> {
+                decoder.$decode(ArrayVisitor::<$t>::new()).await
+            }
+        }
+    };
+}
+
+decode_array!(u8, "byte array", decode_array_u8, visit_array_u8);
+decode_array!(
+    u16,
+    "16-bit unsigned int array",
+    decode_array_u16,
+    visit_array_u16
+);
+decode_array!(
+    u32,
+    "32-bit unsigned int array",
+    decode_array_u32,
+    visit_array_u32
+);
+decode_array!(
+    u64,
+    "64-bit unsigned int array",
+    decode_array_u64,
+    visit_array_u64
+);
+
+decode_array!(i16, "16-bit int array", decode_array_i16, visit_array_i16);
+decode_array!(i32, "32-bit int array", decode_array_i32, visit_array_i32);
+decode_array!(i64, "64-bit int array", decode_array_i64, visit_array_i64);
+
+decode_array!(f32, "32-bit int array", decode_array_f32, visit_array_f32);
+decode_array!(f64, "64-bit int array", decode_array_f64, visit_array_f64);
+
+#[async_trait]
+trait DenseAccessor {
+    type Block: NDArrayRead;
+
+    async fn blocks(self) -> Result<Box<dyn Stream<Item = Result<Self::Block, Error>>>, Error>;
+}
+
+pub struct DenseFile<FE, T> {
+    axis: usize,
+    shape: Vec<usize>,
+    blocks: DirLock<FE>,
+    dtype: PhantomData<T>,
+}
+
+#[async_trait]
+impl<FE, T> DenseAccessor for DenseFile<FE, T>
+where
+    FE: FileLoad + AsType<Array<T>>,
+    T: CDatatype + 'static,
+    Array<T>: de::FromStream<Context = ()>,
+{
+    type Block = ArrayBase<T>;
+
+    async fn blocks(self) -> Result<Box<dyn Stream<Item = Result<Self::Block, Error>>>, Error> {
+        let dir = self.blocks.read().await;
+
+        let block_reads = dir
+            .files()
+            .cloned()
+            .map(|file| file.into_read())
+            .collect::<Vec<_>>();
+
+        let blocks = stream::iter(block_reads)
+            .buffered(num_cpus::get())
+            .map_err(Error::from)
+            .map(move |array| {
+                let array = array?;
+                let block_size = self.shape.iter().rev().take(self.axis).product::<usize>();
+
+                let block_shape = if self.axis == self.shape.len() - 1 {
+                    vec![array.data.len()]
+                } else {
+                    let axis_dim = array.data.len() / block_size;
+                    debug_assert_eq!(array.data.len() % axis_dim, 0);
+
+                    let mut shape = Vec::with_capacity(self.shape.len() - self.axis + 1);
+                    shape.push(axis_dim);
+                    shape.extend(self.shape.iter().skip(self.axis).copied());
+
+                    shape
+                };
+
+                ArrayBase::new(block_shape, array.data.to_vec()).map_err(Error::from)
+            });
+
+        Ok(Box::new(blocks))
+    }
+}
---
+++ src/sparse.rs
@@ -0,0 +1,171 @@
+use std::marker::PhantomData;
+use std::{fmt, io};
+
+use async_trait::async_trait;
+use b_table::{Range, TableLock};
+use futures::{Stream, TryStreamExt};
+use ha_ndarray::CDatatype;
+use number_general::{Number, NumberCollator};
+use safecast::{AsType, CastInto};
+
+use super::{Coord, Error};
+
+const BLOCK_SIZE: usize = 4_096;
+
+pub type Node = b_table::b_tree::Node<Vec<Vec<Number>>>;
+
+#[derive(Clone, Eq, PartialEq)]
+pub struct IndexSchema {
+    columns: Vec<usize>,
+}
+
+impl b_table::b_tree::Schema for IndexSchema {
+    type Error = Error;
+    type Value = Number;
+
+    fn block_size(&self) -> usize {
+        BLOCK_SIZE
+    }
+
+    fn len(&self) -> usize {
+        self.columns.len()
+    }
+
+    fn order(&self) -> usize {
+        12
+    }
+
+    fn validate(&self, key: Vec<Self::Value>) -> Result<Vec<Self::Value>, Self::Error> {
+        if key.len() == self.len() {
+            Ok(key)
+        } else {
+            Err(io::Error::new(io::ErrorKind::InvalidData, "wrong number of values").into())
+        }
+    }
+}
+
+impl b_table::IndexSchema for IndexSchema {
+    type Id = usize;
+
+    fn columns(&self) -> &[Self::Id] {
+        &self.columns
+    }
+
+    fn extract_key(&self, key: &[Self::Value], other: &Self) -> Vec<Self::Value> {
+        debug_assert_eq!(key.len(), self.columns.len());
+        other.columns.iter().copied().map(|x| key[x]).collect()
+    }
+}
+
+impl fmt::Debug for IndexSchema {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("sparse tensor index schema")
+    }
+}
+
+#[derive(Clone, Eq, PartialEq)]
+pub struct Schema {
+    primary: IndexSchema,
+    auxiliary: Vec<(String, IndexSchema)>,
+}
+
+impl Schema {
+    fn ndim(&self) -> usize {
+        self.primary.columns.len() - 1
+    }
+}
+
+impl b_table::Schema for Schema {
+    type Id = usize;
+    type Error = Error;
+    type Value = Number;
+    type Index = IndexSchema;
+
+    fn key(&self) -> &[Self::Id] {
+        &self.primary.columns[..self.ndim()]
+    }
+
+    fn values(&self) -> &[Self::Id] {
+        &self.primary.columns[self.ndim()..]
+    }
+
+    fn primary(&self) -> &Self::Index {
+        &self.primary
+    }
+
+    fn auxiliary(&self) -> &[(String, IndexSchema)] {
+        &self.auxiliary
+    }
+
+    fn validate_key(&self, key: Vec<Self::Value>) -> Result<Vec<Self::Value>, Self::Error> {
+        if key.len() == 1 {
+            Ok(key)
+        } else {
+            let cause = io::Error::new(
+                io::ErrorKind::InvalidData,
+                format!("invalid key: {:?}", key),
+            );
+
+            Err(cause.into())
+        }
+    }
+
+    fn validate_values(&self, values: Vec<Self::Value>) -> Result<Vec<Self::Value>, Self::Error> {
+        if values.len() == 3 {
+            Ok(values)
+        } else {
+            let cause = io::Error::new(
+                io::ErrorKind::InvalidData,
+                format!("invalid values: {:?}", values),
+            );
+
+            Err(cause.into())
+        }
+    }
+}
+
+impl fmt::Debug for Schema {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("sparse tensor schema")
+    }
+}
+
+#[async_trait]
+pub trait SparseAccessor {
+    type DType;
+
+    async fn elements(
+        self,
+    ) -> Result<Box<dyn Stream<Item = Result<(Coord, Self::DType), Error>>>, Error>;
+}
+
+pub struct SparseTable<FE, T> {
+    table: TableLock<Schema, IndexSchema, NumberCollator, FE>,
+    dtype: PhantomData<T>,
+}
+
+#[async_trait]
+impl<FE: AsType<Node> + Send + Sync + 'static, T: CDatatype> SparseAccessor for SparseTable<FE, T>
+where
+    Number: CastInto<T>,
+{
+    type DType = T;
+
+    async fn elements(
+        self,
+    ) -> Result<Box<dyn Stream<Item = Result<(Coord, Self::DType), Error>>>, Error> {
+        let rows = self.table.rows(Range::default(), &[], false).await?;
+        let elements = rows.map_ok(|row| unwrap_row(row)).map_err(Error::from);
+        Ok(Box::new(elements))
+    }
+}
+
+#[inline]
+fn unwrap_row<T>(mut row: Vec<Number>) -> (Coord, T)
+where
+    Number: CastInto<T> + CastInto<u64>,
+{
+    let n = row.pop().expect("n").cast_into();
+    let coord = row.into_iter().map(|i| i.cast_into()).collect();
+    (coord, n)
+}
